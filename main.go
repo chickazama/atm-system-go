2faf44eb74c0cd2c0de982fb43f3e452d72c7211
Matthew Hope <chickazama@gmail.com>
2024-03-29 20:45:13 +0000

    Scaffold main switch block

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"log"
+	"matthewhope/atm-system-go/repo"
 	"matthewhope/atm-system-go/ui"
 )
 
 func main() {
+	r := repo.NewSQLiteRepository()
 	var err error
 	opt := ui.OPT_MAIN_MENU
 	for opt != ui.OPT_EXIT {
 		switch opt {
 		case ui.OPT_MAIN_MENU:
 			opt, err = ui.MainMenu()
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		case ui.OPT_SIGNUP_MENU:
+			opt, err = ui.SignupMenu(r)
 			if err != nil {
 				log.Fatal(err.Error())
 			}
