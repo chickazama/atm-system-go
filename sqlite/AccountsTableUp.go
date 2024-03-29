0d9a738fa82488776856ae841e06570817771c5c
Matthew Hope <chickazama@gmail.com>
2024-03-29 20:09:15 +0000

    Scaffold sqlite.AccountsTableUp

---
+++ sqlite/AccountsTableUp.go
@@ -0,0 +1,23 @@
+package sqlite
+
+import "database/sql"
+
+func AccountsTableUp(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS "ACCOUNTS" (
+		AccountID INTEGER PRIMARY KEY AUTOINCREMENT,
+		CreatedAt BIG INTEGER NOT NULL,
+		AccountNumber TEXT UNIQUE NOT NULL,
+		Balance BIG INTEGER NOT NULL,
+		UpdatedAt BIG INTEGER NOT NULL,
+	);`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec()
+	if err != nil {
+		return err
+	}
+	return nil
+}
