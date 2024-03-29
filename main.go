1e09d2b1ada0d66d0ac196ede14bb27f93640c98
Matthew Hope <chickazama@gmail.com>
2024-03-29 21:08:51 +0000

    Add router, serve static files, listen and serve

--- main.go
+++ main.go
@@ -2,33 +2,26 @@ package main
 
 import (
 	"log"
-	"matthewhope/atm-system-go/repo"
+	"matthewhope/atm-system-go/router"
 	"matthewhope/atm-system-go/ui"
+	"net/http"
+	"regexp"
 )
 
 func main() {
-	r := repo.NewSQLiteRepository()
-	var err error
-	opt := ui.OPT_MAIN_MENU
-	for opt != ui.OPT_EXIT {
-		switch opt {
-		case ui.OPT_MAIN_MENU:
-			opt, err = ui.MainMenu()
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-		case ui.OPT_SIGNUP_MENU:
-			opt, err = ui.SignupMenu(r)
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-		case ui.OPT_PROFILE_MENU:
-			opt, err = ui.ProfileMenu()
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-		default:
-			log.Fatal("option not recognised")
-		}
+	mux := http.NewServeMux()
+	serveStaticFiles(mux)
+	r := router.New()
+	r.AddHandler(regexp.MustCompile(`/$`), ui.NewIndexHandler())
+	mux.Handle("/", r)
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
+}
+
+func serveStaticFiles(mux *http.ServeMux) {
+	fsRoot := http.Dir("./static/")
+	fs := http.FileServer(fsRoot)
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 }
