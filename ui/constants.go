78cc565fed8722af2eb1f96d51dba7a007db231b
Matthew Hope <chickazama@gmail.com>
2024-03-29 20:44:19 +0000

    Define newline delimeter constant and stdin buffered reader

--- ui/constants.go
+++ ui/constants.go
@@ -1,6 +1,18 @@
 package ui
 
+import (
+	"bufio"
+	"os"
+)
+
 const (
-	OPT_EXIT      = 0
-	OPT_MAIN_MENU = 1
+	OPT_EXIT             = 0
+	OPT_MAIN_MENU        = 1
+	OPT_SIGNUP_MENU      = 2
+	OPT_LOGIN_MENU       = 3
+	newlineDelim    byte = '\n'
+)
+
+var (
+	br = bufio.NewReader(os.Stdin)
 )
