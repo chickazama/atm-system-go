c3b3011c12f3a160e522fe1ffd74ab01df1ae594
Matthew Hope <chickazama@gmail.com>
2024-03-30 21:58:38 +0000

    Handle disallowed methods in auth.GithubHandler

--- auth/GithubHandler.go
+++ auth/GithubHandler.go
@@ -12,6 +12,15 @@ func NewGithubHandler() *GithubHandler {
 }
 
 func (h *GithubHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.get(w, r)
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (h *GithubHandler) get(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("https://github.com/login/oauth/authorize?scope=read:user&client_id=%s&client_secret=%s", config.ClientID, config.ClientSecret)
 	http.Redirect(w, r, url, http.StatusSeeOther)
 }
