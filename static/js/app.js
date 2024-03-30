880b5fc3459357d4c87ab815811c2e211654e551
Matthew Hope <chickazama@gmail.com>
2024-03-30 17:43:27 +0000

    Revert app JS to simple console log

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,3 @@
 window.addEventListener("load", async () => {
-    // const main = document.getElementById("main");
-    const body = {
-        emailAddress: "test2@example.com",
-        password: "password123",
-        firstName: "test",
-        lastName: "user2",
-        username: "testUser2"
-    };
-    const res = await fetch("/api/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json" 
-        },
-        body: JSON.stringify(body)
-    })
-    const data = await res.json()
-    console.log(data);
-    // const body = {
-    //     accountNumber: "12345678",
-    //     balance: 100
-    // };
-    // const res = await fetch("/api/accounts", {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json" 
-    //     },
-    //     body: JSON.stringify(body)
-    // })
-    // const data = await res.json()
-    // console.log(data);
+    console.log("Page loaded.");
 })
