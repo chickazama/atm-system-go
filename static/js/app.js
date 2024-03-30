d28a58ffeebb37f4a2311f3946fb5b3f48ba24fe
Matthew Hope <chickazama@gmail.com>
2024-03-30 17:19:56 +0000

    Scaffold API request to Users Handler

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,19 @@
-window.addEventListener("load", () => {
-    console.log("Page loaded.");
+window.addEventListener("load", async () => {
+    // const main = document.getElementById("main");
+    const body = {
+        emailAddress: "test@example.com",
+        password: "password123",
+        firstName: "test",
+        lastName: "user",
+        username: "testUser"
+    };
+    const res = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json" 
+        },
+        body: JSON.stringify(body)
+    })
+    const data = await res.json()
+    console.log(data);
 })
