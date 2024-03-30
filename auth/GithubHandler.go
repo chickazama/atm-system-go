4182321ed9c19b363ffec4623850ed46f6fa9c3d
Matthew Hope <chickazama@gmail.com>
2024-03-30 21:33:03 +0000

    Change GitHub handler scope to read:user

--- auth/GithubHandler.go
+++ auth/GithubHandler.go
@@ -12,6 +12,6 @@ func NewGithubHandler() *GithubHandler {
 }
 
 func (h *GithubHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	url := fmt.Sprintf("https://github.com/login/oauth/authorize?scope=user:email&client_id=%s&client_secret=%s", config.ClientID, config.ClientSecret)
+	url := fmt.Sprintf("https://github.com/login/oauth/authorize?scope=read:user&client_id=%s&client_secret=%s", config.ClientID, config.ClientSecret)
 	http.Redirect(w, r, url, http.StatusSeeOther)
 }
