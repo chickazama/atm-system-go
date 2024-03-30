4135427624ddb30c0788b3e1a45f195f5a25a8d0
Matthew Hope <chickazama@gmail.com>
2024-03-30 17:20:11 +0000

    Register API handlers with router

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"matthewhope/atm-system-go/api"
+	"matthewhope/atm-system-go/repo"
 	"matthewhope/atm-system-go/router"
 	"matthewhope/atm-system-go/ui"
 	"net/http"
@@ -12,7 +14,9 @@ func main() {
 	mux := http.NewServeMux()
 	serveStaticFiles(mux)
 	r := router.New()
+	rp := repo.NewSQLiteRepository()
 	r.AddHandler(regexp.MustCompile(`/$`), ui.NewIndexHandler())
+	addAPIHandlers(r, rp)
 	mux.Handle("/", r)
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
@@ -24,4 +28,9 @@ func serveStaticFiles(mux *http.ServeMux) {
 	fsRoot := http.Dir("./static/")
 	fs := http.FileServer(fsRoot)
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+}
+
+func addAPIHandlers(r *router.Router, rp repo.IRepository) {
+	r.AddHandler(regexp.MustCompile(`^/api/users$`), api.NewUsersHandler(rp))
+	r.AddHandler(regexp.MustCompile(`^/api/accounts$`), api.NewAccountsHandler(rp))
 }
