f20daccc8918613e76676beece314cf7ce85e585
Matthew Hope <chickazama@gmail.com>
2024-03-30 21:33:26 +0000

    Get OAuth Bearer Token on auth.Callback

--- auth/CallbackHandler.go
+++ auth/CallbackHandler.go
@@ -2,6 +2,8 @@ package auth
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -26,7 +28,28 @@ func (h *CallbackHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (h *CallbackHandler) get(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query().Get("code")
 	fmt.Println(code)
-	url := fmt.Sprintf("/?code=%s", code)
+	url := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", config.ClientID, config.ClientSecret, code)
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	req.Header.Set("Accept", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	buf, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%s\n", buf)
+	url = "/"
 	cookie := http.Cookie{
 		Name:     "Session",
 		Value:    "TestValue",
