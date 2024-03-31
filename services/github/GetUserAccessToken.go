a9ea0f9a612421a5f02b55ed3776c54d178d4c4c
Matthew Hope <chickazama@gmail.com>
2024-03-31 19:53:17 +0100

    Return OAuthToken value in github.GetUserAccessToken function

--- services/github/GetUserAccessToken.go
+++ services/github/GetUserAccessToken.go
@@ -1,29 +1,36 @@
 package github
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
-func GetUserAccessToken(code string, config OAuthConfig) {
+func GetUserAccessToken(code string, config OAuthConfig) (string, error) {
 	url := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", config.ClientID, config.ClientSecret, code)
 	req, err := http.NewRequest(http.MethodPost, url, nil)
 	if err != nil {
 		log.Println(err.Error())
-		return
+		return "", err
 	}
 	req.Header.Set("Accept", "application/json")
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Println(err.Error())
-		return
+		return "", err
 	}
 	buf, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Println(err.Error())
-		return
+		return "", err
 	}
-	fmt.Printf("%s\n", buf)
+	var token OAuthToken
+	err = json.Unmarshal(buf, &token)
+	if err != nil {
+		log.Println(err.Error())
+		return "", err
+	}
+	return token.AccessToken, nil
 }
